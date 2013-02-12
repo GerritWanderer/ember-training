611d86eab36e91c253a84009646d78b096e99e3c
tomhuda <tomhuda@tilde.io>
2013-02-11 20:24:23 -0800

    Step 8

--- js/app.js
+++ js/app.js
@@ -12,6 +12,13 @@ App.Router.map(function() {
   this.resource('album', { path: '/album/:album_id' });
 });
 
+Ember.Handlebars.registerBoundHelper('format-duration', function(seconds) {
+  var formattedMinutes = Math.floor(seconds / 60);
+  var formattedSeconds = seconds % 60;
+  formattedSeconds = formattedSeconds < 10 ? "0" + formattedSeconds : formattedSeconds;
+  return formattedMinutes + ":" + formattedSeconds;
+});
+
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return App.Album.find();
