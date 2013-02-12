532cbc59f0b840a416bd4e432316951a3fc5e19e
tomhuda <tomhuda@tilde.io>
2013-02-11 20:26:42 -0800

    Step 9

--- js/app.js
+++ js/app.js
@@ -12,6 +12,19 @@ App.Router.map(function() {
   this.resource('album', { path: '/album/:album_id' });
 });
 
+App.AlbumController = Ember.ObjectController.extend({
+  totalDuration: function() {
+    var songs = this.get('songs'),
+        total = 0;
+
+    songs.forEach(function(song) {
+      total += song.get('duration');
+    });
+
+    return total;
+  }.property('songs.@each.duration')
+});
+
 Ember.Handlebars.registerBoundHelper('format-duration', function(seconds) {
   var formattedMinutes = Math.floor(seconds / 60);
   var formattedSeconds = seconds % 60;
