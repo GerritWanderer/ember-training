842e38ff578cc018f77cc83c1af10ffa98454bf3
tomhuda <tomhuda@tilde.io>
2013-02-11 20:36:26 -0800

    Step 10

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ App.Router.map(function() {
 });
 
 App.AlbumController = Ember.ObjectController.extend({
+  needs: ['nowPlaying'],
   totalDuration: function() {
     var songs = this.get('songs'),
         total = 0;
@@ -22,8 +23,14 @@ App.AlbumController = Ember.ObjectController.extend({
     });
 
     return total;
-  }.property('songs.@each.duration')
+  }.property('songs.@each.duration'),
+
+  play: function(song) {
+    this.set('controllers.nowPlaying.model', song);
+  }
 });
+
+App.NowPlayingController = Ember.ObjectController.extend();
 
 Ember.Handlebars.registerBoundHelper('format-duration', function(seconds) {
   var formattedMinutes = Math.floor(seconds / 60);
